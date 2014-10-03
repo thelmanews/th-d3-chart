46870b064a706b7e8647c1f48f3b2f8232f0e5b5
Nisha Batra <nishabatra27@gmail.com>
2014-10-03 17:15:58 -0400

    remove commentsg

--- utility.js
+++ utility.js
@@ -364,17 +364,12 @@ Thelma.chartUtils = {
     },
     hexToRgb: function(hex){
       var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      // if (returnType === "obj"){
-        return result ? {
-          r: parseInt(result[1], 16),
-          g: parseInt(result[2], 16),
-          b: parseInt(result[3], 16)
-        } : null;  
-      // } else {
-      //   // console.log(parseInt(result[1], 16).toString());
-      //   return result ? (parseInt(result[1], 16)) + "," + (parseInt(result[2], 16)) + "," + (parseInt(result[3], 16)) : null;
-      // }
-      
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null;  
+
     }
 
 }
